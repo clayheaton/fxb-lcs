6a161529236b3e3331c161dd4ad2e097c4b8ab76
Clay Heaton <ccheaton@gmail.com>
2018-02-20 21:09:11 -0500

    merged link harvesting with scraping of content pages

--- vdc_scraper.py
+++ vdc_scraper.py
@@ -8,6 +8,9 @@ import requests
 import dataset
 import json
 
+# The last page of Arabic search results plus 1
+IDX_END_AR = 1799
+
 
 def get_content_table(link):
     """
@@ -26,12 +29,62 @@ def get_content_table(link):
         return str(table)
 
 
+def extract_links_from_results_page(lang, idx, code):
+    """
+    Pulls the HTML from a search results page on the VDC and returns a list of links
+    """
+    links = []
+    uri = "http://www.vdc-sy.info/index.php/" + lang + "/martyrs/" + str(idx) + "/" + code
+    result = requests.get(uri)
+    soup = BeautifulSoup(result.content,"lxml")
+    for link in soup.find_all("a"):
+        href = link.get("href")
+        if "/details/martyrs/" in href:
+            martyr_link = "http://www.vdc-sy.info" + href
+            links.append(martyr_link)
+
+    return links
+
+
+def get_links_from_search(db, tab):
+    """
+    Scrapes the search results page to get the links that we
+    need to scrape to get the entire data set.
+    """
+    code = "c29ydGJ5PWEua2lsbGVkX2RhdGV8c29ydGRpcj1ERVNDfGFwcHJvdmVkPXZpc2libGV8ZXh0cmFkaXNwbGF5PTB8"
+    link_list_ar = []
+
+    for num in range(1, IDX_END_AR):
+        links = extract_links_from_results_page('ar', num, code)
+        link_list_ar = link_list_ar + links
+        sleep(1)
+        if num % 100 == 0:
+            print("Results page", num)    
+
+        if num == IDX_END_AR - 1:
+            print("LAST RESULTS PAGE:", num)
+
+    print(len(link_list_ar), "LINKS HARVESTED")
+    for link in link_list_ar:
+        en_link = link.replace("/ar/", "/en/")
+        rec = {"ar_link": link, "end_link": en_link}
+        tab.insert(rec)
+
 def main():
     """
     Scrape the VDC
     """
+
     db = dataset.connect("sqlite:///vdc.sqlite")
     tab = db['links']
+
+    # Skip if we've already harvested links
+    # HOWEVER we don't know how many to harvest
+    # Check nohup.out to make sure we don't need to start again.
+    if tab.count == 0:
+        # Populate the links table
+        get_links_from_search(db, tab)
+
     tab_ar = db['content_ar']
     tab_en = db['content_en']
 
